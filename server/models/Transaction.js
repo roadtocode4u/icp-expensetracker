373471ea636e01ca50359d9b9e18cf402d486599
surajshende247 <surajshende247@gmail.com>
2024-07-10 18:50:33 +0530

    Models Added

---
+++ server/models/Transaction.js
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose";
+
+const transactionSchema = new Schema({
+  amount: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    default: "others",
+  },
+  type: {
+    type: String,
+    enum: ["debit", "credit"],
+  },
+  user:{
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  }
+},{
+  timestamps: true,
+});
+
+const Transaction = model("Transaction", transactionSchema);
+
+export default Transaction;
